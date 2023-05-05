c83f03ff71e43e9c93be67d7db4c3f931acad808
wangjiacun <wangjiacun@meituan.com>
2023-05-05 15:44:00 +0800

    update code10

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QApplication, QWidget, QMainWindow
+from PySide6 import QtCore, QtWidgets
 
 # from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 # from PyQt5.QtCore import QEventLoop
@@ -39,38 +40,29 @@ class Window(QWidget):
 
     async def btnEvent(self):
         print("the ok btn is clicked")
-        print("combox is ", self.ui.combox.currentText())
-        print("speed is ", self.ui.speed.value())
-        print("volumn is ", self.ui.volumn.value())
-        print("text is ", self.ui.text.toPlainText())
-        self.ui.text.setPlainText("")
-        self.ui.speed.setValue(0)
-        self.ui.volumn.setValue(0)
+        # print("combox is ", self.ui.combox.currentText())
+        # print("speed is ", self.ui.speed.value())
+        # print("volumn is ", self.ui.volumn.value())
+        # print("text is ", self.ui.text.toPlainText())
         self.tts.set_text(self.ui.text.toPlainText())
         self.tts.set_voice(self.ui.combox.currentText())
         self.tts.set_output_name("test")
+        
+        self.ui.text.setPlainText("")
+        self.ui.speed.setValue(0)
+        self.ui.volumn.setValue(0)
+        
         self._task = asyncio.create_task(self.ttsEvent(self.tts))
-        print("task is ", self._task)
         res = await self._task
         print("res is ", res)
-        # communicate = edge_tts.Communicate("协程(Coroutine)本质上是一个函数，特点是在代码块中可以将执行权交给其他协程", "zh-CN-XiaoxiaoNeural")
-        # await communicate.save("./data/" + str("test") + ".mp3")
+        self.ui.ok.setDisabled(0)
+        # if res == "OK":
+            # self.ui.ok.setEnabled(1)
+            # app.aboutQt()
         
     async def ttsEvent(self, tts):
-        text = """
-            协程(Coroutine)本质上是一个函数，特点是在代码块中可以将执行权交给其他协程
-            众所周知，子程序（函数）都是层级调用的，如果在A中调用了B，那么B执行完毕返回后A才能执行完毕。协程与子程序有点类似，但是它在执行过程中可以中断，转而执行其他的协程，在适当的时候再回来继续执行。
-            协程与多线程相比的最大优势在于：协程是一个线程中执行，没有线程切换的开销；协程由用户决定在哪里交出控制权
-            这里用到的是asyncio库(Python 3.7)，这个库包含了大部分实现协程的魔法工具
-        """
-
-        print("tts event 任务启动")
-        print(dir(edge_tts))
-        # com = Communicate(text, "zh-CN-XiaoxiaoNeural")
-        # print(com)
-        communicate = edge_tts.Communicate(text, "zh-CN-XiaoxiaoNeural")
-        await communicate.save("./data/" + str("test") + ".mp3")
-        await asyncio.sleep(2)
+        communicate = edge_tts.Communicate(self.tts.get_text(), self.tts.get_voice())
+        await communicate.save("./data/" + str(self.tts.get_output_name()) + ".mp3")
         return "OK"
 
 
@@ -81,6 +73,6 @@ if __name__ == "__main__":
     # loop = QEventLoop(app)
     # asyncio.set_event_loop(loop)
     win = Window()
-    with qtinter.using_asyncio_from_qt():  # <-- enable asyncio in qt code
+    with qtinter.using_asyncio_from_qt(): 
         win.show()
         sys.exit(app.exec())
